f0f99118a72b645e23325d3ac2873bb2129866e8
ftech-glitch <feliciattan@gmail.com>
2024-03-15 12:45:28 +0800

    remove brewerieslist component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Details.module.css";
-import BreweriesList from "./BreweriesList";
 
 const Home = () => {
   const [randomBrewery, setRandomBrewery] = useState(null);
